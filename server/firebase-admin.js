f5eff1134207145e33acaaec71145136941177f2
Abayili Consulting <clubdesinvestisseurs4@gmail.com>
2026-06-25 12:07:21 +0000

    update app

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,12 +1,20 @@
 const admin = require('firebase-admin');
 
+function loadCredential() {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+    const json = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
+    return admin.credential.cert(JSON.parse(json));
+  }
+  return admin.credential.cert({
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  });
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId:   process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey:  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+    credential: loadCredential(),
   });
 }
 
